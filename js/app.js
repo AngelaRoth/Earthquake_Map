db3bf489745e6db0dc38a6f3503e4af484305396
Angela <Angela>
2017-04-02 15:20:41 -0400

    Markers show up using Fixed URL for Earthquake API

---
+++ js/app.js
@@ -0,0 +1,175 @@
+var Quake = function(data) {
+  this.place = ko.observable(data.place);
+  this.location = ko.observable(data.location);
+  this.time = ko.observable(data.time);
+  this.magnitude = ko.observable(data.magnitude);
+  this.alert = ko.observable(data.alert);
+  this.tsunami = ko.observable(data.tsunami);
+}
+
+var ViewModel = function() {
+  var self = this;
+  self.googleReady = ko.observable(false);
+  self.quakesLoaded = ko.observable(false);
+  self.quakeArray = ko.observableArray([]);
+
+  this.makeMarkers = ko.computed(function() {
+    if (self.googleReady() && self.quakesLoaded()) {
+      var largeInfowindow = new google.maps.InfoWindow();
+      var bounds = new google.maps.LatLngBounds();
+
+      self.quakeArray().forEach(function(item) {
+        var iconColor = getColor(item.magnitude())
+        var icon = makeMarkerIcon(iconColor);
+        /*var icon = makeMarkerIcon('7BB718');*/
+        item.marker = new  google.maps.Marker({
+          map: map,
+          position: item.location(),
+          title: item.place(),
+          icon: icon
+        });
+
+        item.marker.addListener('click', function() {
+          self.populateInfoWindow(this, largeInfowindow);
+        });
+
+        bounds.extend(item.marker.position);
+
+      });
+
+      map.fitBounds(bounds);
+    }
+  }, this);
+
+  // Thanks to StackOverflow for suggesting how to trigger any Maps API event listener using the event.trigger function
+  // http://stackoverflow.com/questions/9194579/how-to-simulate-a-click-on-a-google-maps-marker
+  this.listItemClicked = function() {
+    google.maps.event.trigger(this.marker, 'click', {
+      latLng: new google.maps.LatLng(0,0)
+    });
+  }
+/*
+  this.filter = function(type) {
+    var bounds = new google.maps.LatLngBounds();
+    self.filteredList().length = 0;
+    self.locationList().forEach(function(item) {
+      if (item.type() === type || type === 'all') {
+        self.filteredList.push(item);
+        item.marker.setMap(map);
+        bounds.extend(item.marker.position);
+      } else {
+        item.marker.setMap(null);
+      }
+    });
+    self.allBounds = bounds;
+    map.fitBounds(bounds);
+  };
+*/
+  this.loadEarthquakes = function() {
+    var earthquakeURL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=1900-01-01&endtime=1907-04-01&minmagnitude=7&maxmagnitude=8';
+
+    console.log('earthquakeURL = ' + earthquakeURL);
+
+    $.getJSON( earthquakeURL )
+      .done(function(data) {
+        // log data to see how it's structured.
+        console.log(data);
+
+        if (data.hasOwnProperty('features')) {
+          var features = data.features;
+          if (features.length > 0) {
+            features.forEach(function(e) {
+              var quakeObject = {
+                place: e.properties.place,
+                location: {
+                  lat: e.geometry.coordinates[1],
+                  lng: e.geometry.coordinates[0]
+                },
+                time: e.properties.time,
+                magnitude: e.properties.mag,
+                alert: e.properties.alert,//green
+                tsunami: e.properties.tsunami
+              };
+
+              var newQuake = new Quake(quakeObject);
+              self.quakeArray.push(newQuake);
+            });
+
+            self.quakesLoaded(true);
+
+          } else {
+            console.log('no quakes found');
+          }
+        }
+
+
+/*
+        if (data.hasOwnProperty('items')) {
+          bookFound = true;
+          var items = data.items;
+          var firstBook = data.items[0];
+          console.log(firstBook);
+        }
+*/
+/*
+        if (bookFound) {
+          // Check if properties exist and assign their values to global variables
+          if (firstBook.volumeInfo.hasOwnProperty('title')) {
+            bookTitle = firstBook.volumeInfo.title;
+          } else {
+            bookTitle = "No Title Found";
+          }
+          if (firstBook.volumeInfo.hasOwnProperty('authors')) {
+            bookAuthor = "";
+            var authors = firstBook.volumeInfo.authors;
+            var numAuthors = authors.length;
+            for (var i = 0; i < (numAuthors - 1); i++) {
+              bookAuthor += authors[i];
+              bookAuthor += ', ';
+            }
+            bookAuthor += authors[numAuthors - 1];
+          } else {
+            bookAuthor = "No Author Listed";
+          }
+          if (firstBook.volumeInfo.hasOwnProperty('imageLinks')) {
+            if (firstBook.volumeInfo.imageLinks.hasOwnProperty('smallThumbnail')) {
+              bookImageSrc = firstBook.volumeInfo.imageLinks.smallThumbnail;
+            } else if (firstBook.volumeInfo.imageLinks.hasOwnProperty('thumbnail')) {
+              bookImageSrc = firstBook.volumeInfo.imageLinks.thumbnail;
+            }
+          } else {
+            bookImageSrc = "img/books.jpg";
+          }
+        } else {
+          bookTitle = 'No Title Found';
+          bookAuthor = 'No Author Listed';
+          bookImageSrc = 'img/books.jpg';
+        }
+*/
+      })
+      .fail(function() {
+        console.log('Earthquake data Unavailable');
+      });
+
+    return false;
+  };
+};
+
+
+function getColor(mag) {
+  if (mag >= 8.0) {
+    return ('ff0000');
+  } else if (mag >= 7.7) {
+    return ('ffa500');
+  } else if (mag >= 7.2) {
+    return ('ffff24');
+  } else {
+    return ('7bb718');
+  }
+}
+
+var viewModel = new ViewModel();
+
+ko.applyBindings(viewModel);
+
+viewModel.loadEarthquakes();
---
+++ js/map.js
@@ -0,0 +1,59 @@
+var map;
+
+// Create a new blank array for all the listing markers.
+var markers = [];
+
+ViewModel.prototype.initMap = function() {
+  // Constructor creates a new map - only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 43.5425, lng: -80.24466},
+    zoom: 15,
+    mapTypeControl: false
+  });
+
+  /*var largeInfowindow = new google.maps.InfoWindow();*/
+  var bounds = new google.maps.LatLngBounds();
+
+  map.fitBounds(bounds);
+
+  this.googleReady(true);
+};
+
+ViewModel.prototype.populateInfoWindow = function(marker, infowindow) {
+  if (infowindow.marker != marker) {
+    infowindow.marker = marker;
+    infowindow.setContent(marker.title);
+    infowindow.open(map, marker);
+    // Make sure the marker property is cleared if the infowindow is closed.
+    infowindow.addListener('closeclick', function(){
+      infowindow.setMarker = null;
+    });
+  }
+}
+
+// This function takes in a COLOR, and then creates a new marker
+// icon of that color. The icon will be 21 px wide by 34 high, have an origin
+// of 0, 0 and be anchored at 10, 34).
+
+function makeMarkerIcon(markerColor) {
+  var markerImage = new google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+    '|40|_|%E2%80%A2',
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+    new google.maps.Size(21, 34));
+  return markerImage;
+}
+/*
+function makeBigIcon(markerColor) {
+  var markerImage = new google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+    '|40|_|%E2%80%A2',
+    new google.maps.Size(42, 68),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(21, 50),
+    new google.maps.Size(42, 68));
+  return markerImage;
+}
+*/
